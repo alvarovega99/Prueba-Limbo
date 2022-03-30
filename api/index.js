39c7d46dff1949986d41fa86a8895c836df79520
alvarovega99 <vegaa8238@gmail.com>
2022-03-30 12:51:18 -0300

    arreglos

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 const server = require('./src/app.js')
 // Sincronizar todos los modelos a la vez
-server.listen(3008, () => {
+server.listen(3001, () => {
   console.log('Servidor corriendo en el puerto 3001')
 })
--- api/src/routes/Prueba.js
+++ api/src/routes/Prueba.js
@@ -1,24 +1,21 @@
 const express = require('express')
 const router = express.Router()
-const ccxt = require('ccxt')
 const axios = require('axios')
+const ccxt = require('ccxt')
 const kraken = new ccxt.kraken()
-
 router.get('/ventas', async (req, res) => {
   const precioUsd = await kraken.fetchTicker('BTC/USD')
-  console.log(precioUsd.bid)
   try {
-    const ventas = await axios.get('https://localbitcoins.com/buy-bitcoins-online/CO/{country_name=COLOMBIA}/.json')
-    const ventasCop = ventas.data.data.ad_list.filter(venta => venta.data.currency === 'COP')
-    const ventasMostrar = ventasCop.map(venta => {
+    const ventas = await axios.get('https://localbitcoins.com/sell-bitcoins-online/cop/.json')
+    const ventasMostrar = ventas.data.data.ad_list.map(venta => {
       const obj = {
         name: venta.data.profile.username,
         precio: venta.data.temp_price / precioUsd.bid,
-        max:new Intl.NumberFormat('de-DE').format(venta.data.max_amount)
+        max: new Intl.NumberFormat('de-DE').format(venta.data.max_amount)
       }
       return obj
     })
-    res.json(ventasMostrar)
+    res.status(200).json(ventasMostrar)
   } catch (err) {
     res.send(err.message)
   }
@@ -26,17 +23,17 @@ router.get('/ventas', async (req, res) => {
 router.get('/compras', async (req, res) => {
   const precioUsd = await kraken.fetchTicker('BTC/USD')
   try {
-    const compras = await axios.get('https://localbitcoins.com/sell-bitcoins-online/CO/{country_name=COLOMBIA}/.json')
-    const comprasCop = compras.data.data.ad_list.filter(compra => compra.data.currency === 'COP')
-    const comprasMostrar = comprasCop.map(compra => {
+    const compras = await axios.get('https://localbitcoins.com/buy-bitcoins-online/cop/.json')
+    /* const comprasCop = compras.data.data.ad_list.filter(compra => compra.data.currency === 'COP') */
+    const comprasMostrar = compras.data.data.ad_list.map(compra => {
       const obj = {
         name: compra.data.profile.username,
         precio: compra.data.temp_price / precioUsd.bid,
-        max:new Intl.NumberFormat('de-DE').format(compra.data.max_amount)
+        max: new Intl.NumberFormat('de-DE').format(compra.data.max_amount)
       }
       return obj
     })
-    res.json(comprasMostrar)
+    res.status(200).json(comprasMostrar)
   } catch (err) {
     res.send(err.message)
   }
