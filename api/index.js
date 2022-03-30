aa27565b51af7030f36d85af25580911ac85c0c2
alvarovega99 <vegaa8238@gmail.com>
2022-03-30 12:20:54 -0300

    rutas

--- api/index.js
+++ api/index.js
@@ -1,6 +1,5 @@
 const server = require('./src/app.js')
 // Sincronizar todos los modelos a la vez
-server.listen(3001, () => {
+server.listen(3008, () => {
   console.log('Servidor corriendo en el puerto 3001')
 })
-
--- api/src/routes/Prueba.js
+++ api/src/routes/Prueba.js
@@ -1,9 +1,42 @@
 const express = require('express')
 const router = express.Router()
+const ccxt = require('ccxt')
+const axios = require('axios')
+const kraken = new ccxt.kraken()
 
-router.get('/', async (req, res) => {
+router.get('/ventas', async (req, res) => {
+  const precioUsd = await kraken.fetchTicker('BTC/USD')
+  console.log(precioUsd.bid)
   try {
-    res.send('hola mundo')
+    const ventas = await axios.get('https://localbitcoins.com/buy-bitcoins-online/CO/{country_name=COLOMBIA}/.json')
+    const ventasCop = ventas.data.data.ad_list.filter(venta => venta.data.currency === 'COP')
+    const ventasMostrar = ventasCop.map(venta => {
+      const obj = {
+        name: venta.data.profile.username,
+        precio: venta.data.temp_price / precioUsd.bid,
+        max:new Intl.NumberFormat('de-DE').format(venta.data.max_amount)
+      }
+      return obj
+    })
+    res.json(ventasMostrar)
+  } catch (err) {
+    res.send(err.message)
+  }
+})
+router.get('/compras', async (req, res) => {
+  const precioUsd = await kraken.fetchTicker('BTC/USD')
+  try {
+    const compras = await axios.get('https://localbitcoins.com/sell-bitcoins-online/CO/{country_name=COLOMBIA}/.json')
+    const comprasCop = compras.data.data.ad_list.filter(compra => compra.data.currency === 'COP')
+    const comprasMostrar = comprasCop.map(compra => {
+      const obj = {
+        name: compra.data.profile.username,
+        precio: compra.data.temp_price / precioUsd.bid,
+        max:new Intl.NumberFormat('de-DE').format(compra.data.max_amount)
+      }
+      return obj
+    })
+    res.json(comprasMostrar)
   } catch (err) {
     res.send(err.message)
   }
